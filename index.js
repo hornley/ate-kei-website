10cac98e8ec90659b79581ce2901f6e52f8e91d3
hornley <buendiaharley@gmail.com>
2024-03-21 17:32:44 +0800

    3.0

--- index.js
+++ index.js
@@ -5,16 +5,26 @@ let currentBetNumber = 1;
 let agentBettors = {};
 let jsonifyThis = [];
 
+/*
+- Change MOP (Dropdown) to Select Option
+*/
 
 function submit() {
     const agentInput = document.getElementById('Agent');
     const textArea = document.getElementById('Bets');
-    const mopDropdown = document.getElementById('MOP');
     const combinationsText = textArea.value;
     const Array = combinationsText.split(/\n/);
     const Agent = agentInput.value;
-    const mop = mopDropdown.value;
-    // const Agent = 'Yumi';
+    let mop;
+
+    if (document.getElementById('gcash').checked) mop = "GCASH";
+    else if (document.getElementById('stc').checked) mop = "STC";
+
+    if (!Agent || !textArea || !mop) {
+        window.alert("You have a missing required information/s!");
+        return;
+    }
+
     dictionary[Agent] = {};
     
     for (let line in Array) {
@@ -24,17 +34,16 @@ function submit() {
             dictionary[Agent][currentName] = [];
         } else if (text.includes("=")) {
             let combination = text.substring(0, text.indexOf("="));
-            combination = (combination.includes(" ")) ? combination.replace(" ", "-") : combination;
+            combination = combinationValidation(combination);
             let type = (text.includes("S") || text.includes("s")) ? 'Straight' : 'Ramble';
             let amount = text.substring(text.indexOf("=") + 1, text.length - 1);
             let date = new Date();
             let newDate = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
-            dictionary[Agent][currentName].push({'Combination': combination ,'Type': type, 'Amount': amount, 'Date': newDate, 'Mode': mop});
-            jsonifyThis.push({'Agent': Agent, 'Bettor': currentName, 'N1': combination[0], 'N2': combination[2], 'N3': combination[4], 'Amount': amount, 'Type': type, 'Mode': mop, 'Combination': combination, 'Date': newDate});
+            dictionary[Agent][currentName].push({'Combination': `${combination[0]}-${combination[1]}-${combination[2]}` ,'Type': type, 'Amount': amount, 'Date': newDate, 'Mode': mop});
+            jsonifyThis.push({'Agent': Agent, 'Bettor': currentName, 'N1': combination[0], 'N2': combination[1], 'N3': combination[2], 'Amount': amount, 'Type': type, 'Mode': mop, 'Combination': combination, 'Date': newDate});
             numberOfBets++;
         }
     }
-    console.log(jsonifyThis);
     table();
 }
 
@@ -59,30 +68,42 @@ function table() {
     }
 }
 
-function jsonify(el) {
-    let data = "text/json; charset=utf-8," + encodeURIComponent(JSON.stringify(jsonifyThis));
-    el.setAttribute("href", "data:"+data);
-    el.setAttribute("download", "data.json");
+/*
+@Auchi B. A
+0-0-1=30S
+0-1-5=50R
+*/
+
+function csvConvert(el) {
+    let csvRows = [];
+    const headers = ["Agent", "Bettor", "N1", "N2", "N3", "Amount", "Type", "Mode", "Combination", "Date"];
+    csvRows.push(headers.join(','));  
+    for (let bet in jsonifyThis) {
+        const values = Object.values(jsonifyThis[bet]).join(','); 
+        csvRows.push(values);
+    }
+
+    let csvContent = '';
+    csvRows.forEach(row => {
+        csvContent += row + '\n';
+    });
+    
+    const blob = new Blob([csvContent], { type: 'text/csv' }); 
+    const url = window.URL.createObjectURL(blob) 
+    el.setAttribute("href", url);
+    el.setAttribute("download", "data.csv");
+}
+
+function combinationValidation(combination) {
+    let combinations = "";
+    for (let x in combination) {
+        if (parseInt(combination[x]) || parseInt(combination[x]) === 0) { combinations += combination[x]; }
+    }
+    return combinations;
 }
 
 /*
 @Auchi B. A
 0-0-1=30S
 0-1-5=50R
-
-            const cell1 = document.createElement('td');
-            const cell2 = document.createElement('td');
-            const cell3 = document.createElement('td');
-            const cell4 = document.createElement('td');
-            const cell5 = document.createElement('td');
-            const cell6 = document.createElement('td');
-            const cell7 = document.createElement('td');
-            const cell1Text = document.createTextNode(currentBetNumber);
-            const cell2Text = document.createTextNode(currentBetNumber);
-            const cell3Text = document.createTextNode(currentBetNumber);
-            const cell4Text = document.createTextNode(currentBetNumber);
-            const cell5Text = document.createTextNode(currentBetNumber);
-            const cell6Text = document.createTextNode(currentBetNumber);
-            const cell7Text = document.createTextNode(currentBetNumber);
-
 */
